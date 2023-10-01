435456f385065aa86da5f016ca0efc92813ef422
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2023-10-01 19:02:37 +0000

    Making a Java Rock Paper Sissors game

--- Code /Java/RockPaperSissors.java
+++ Code /Java/RockPaperSissors.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 class RockPaperSissors {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         int compMove = 0;
+        int playerIntMove = 0;
 
         String playerMove;
 
+        // 0 - rock, 1 - paper, 2 sissors
         System.out.println("Rock Paper Sissors\n");
         System.out.print("What will you play? ");
 
         playerMove = input.nextLine();
         playerMove = playerMove.toLowerCase();
 
-        if(!(playerMove.equals("rock") || playerMove.equals("paper") || playerMove.equals("sissors"))) {
+        while(!(playerMove.equals("rock") || playerMove.equals("paper") || playerMove.equals("sissors"))) {
             System.out.println("You must chose Rock, Paper, or, Sissors");
+
+            System.out.print("What will you play? ");
+            playerMove = input.nextLine();
+            playerMove = playerMove.toLowerCase();
         }
 
         compMove =  (int)(Math.random() * 3);
+        System.out.println(compMove);
         
-        
+
     }
 }
