492acab0cc680b84d83dc4e1e4f0116d498ca014
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2023-10-02 01:51:36 +0000

    Starting to work on python rock paper sissors

--- Code /Python/RockPaperSissors.py
+++ Code /Python/RockPaperSissors.py
@@ -1,12 +1,16 @@
+"""This is here so Python stops yelling at me"""
 print("Hello World")
 
 print()
-mainGame()
 
-def startAgain():
-    mainGame()
+def startagain():
+    """This, when true, will restart the game"""
+    maingame()
 
-def mainGame():
+def maingame():
+    """The main game"""
     print("Rock Paper Sissors") 
     print()
     playerInput = input("What will you play? ")
+
+maingame()
