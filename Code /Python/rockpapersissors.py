fbe18ca525ed8218e3d5efe987896770a15ef11a
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2023-10-02 13:46:43 +0000

    work

--- Code /Python/rockpapersissors.py
+++ Code /Python/rockpapersissors.py
@@ -1,9 +1,11 @@
 """This is here so Python stops yelling at me"""
-print("Hello World")
+import random
+print("Rock Paper Sissors")
 
 print()
 
 STR_TESTING = "This is a test"
+
 
 def has_number(input_string):
     """Checks if a string has a digit in it"""
@@ -15,16 +17,42 @@ def start_again():
 
 def main_game():
     """The main game"""
-    print("Rock Paper Sissors")
-    print()
     player_input = input('What will you play? ')
+
     while has_number(player_input):
         print("You must chose Rock, Paper, or, Sissors!\n")
         player_input = input('What will you play? ')
+        player_input = player_input.lower()
+
     while not(player_input == "rock" or player_input == "paper" or player_input == "sissors"):
         print("You must chose Rock, Paper, or, Sissors!\n")
         player_input = input('What will you play? ')
+        player_input = player_input.lower()
+
+    comp_move = random.randint(0,2)
+    #0 - rock 1 - paper 2 - sissors 
+
+    if player_input == "rock":
+        if comp_move == 0:
+            print("Computer plays rock \nTie!")
+        elif comp_move == 1:
+            print("Computer plays paper \nComputer wins!")
+        else:
+            print("Computer plays sissors \nYou Win!")
+    elif player_input == "paper":
+        if comp_move == 0:
+            print("Computer plays rock \nYou Win!")
+        elif comp_move == 1:
+            print("computer plays paper \nTie!")
+        else:
+            print("Computer plays sissors \nComputer Wins!")
+    else:
+        if comp_move == 0:
+            print("Computer plays rock \nCompou")
+        elif comp_move == 1:
+            print()
+        else:
+            print()
+
 
 main_game()
-
-print("why")
